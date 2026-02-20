999b2552d3bd87026a6bb40b07c2b64122255fa2
julia.dwyer <julia.dwyer@MU-C1-12.mullen.pvt.k12.co.us>
2026-02-20 14:54:33 -0700

    print main menu

--- SIS Project/src/SISRunner.java
+++ SIS Project/src/SISRunner.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class SISRunner
 	{
@@ -6,7 +7,26 @@ public class SISRunner
 			{
 				//Welcome to group project - Thomas,JT, Julia, Feliciana.
 				// You rock
+				
+				Scanner userIntInput = new Scanner(System.in);
+				System.out.println("What would you like to do?");
+				System.out.println("1) add or delete student");
+				System.out.println("2) change student grades/schedule");
+				System.out.println("3) sort students");
+				int choice = userIntInput.nextInt();
+				
+				if(choice==1) {
+					//need to decide if we want to put submenus in specific classes or methods or not
+					System.out.println("1");
+				}
+				else if(choice==2) {
+					System.out.println("2");
+				}
+				else if(choice == 3) {
+					System.out.println("3");
+				}
 
 			}
 
+	
 	}

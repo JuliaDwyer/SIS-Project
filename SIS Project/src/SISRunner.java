2f4d66a2de7a05ad00e77ac9df154c391f842349
Julia Dwyer <julia.dwyer@mullen.pvt.k12.co.us>
2026-03-03 15:08:31 -0700

    added sorting

--- SIS Project/src/SISRunner.java
+++ SIS Project/src/SISRunner.java
@@ -1,12 +1,13 @@
+import java.util.Scanner;
 
 public class SISRunner
 	{
 
 		public static void main(String[] args)
 			{
-				//Welcome to group project - Thomas,JT, Julia, Feliciana.
-				// You rock
+				
 
 			}
 
+	
 	}
--- SIS Project/src/Sorting.java
+++ SIS Project/src/Sorting.java
@@ -1,5 +1,15 @@
+import java.util.Collections;
+import java.util.Comparator;
 
-public class Sorting
+public class Sorting implements Comparator<Student>
 	{
-
+		public int compare(Student s1, Student s2)
+			{
+int result = 0;
+return 0;
+			}
+		public static void sortClassroom()
+		{
+			Collections.sort(students, new Sorting());
+		}
 	}
